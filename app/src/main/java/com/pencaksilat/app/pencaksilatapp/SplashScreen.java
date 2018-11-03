5ee1cf4944261c52b15ba4d33d199a150a94fccd
Juris Vassa Ivandro <juris@unej.ac.id>
2018-11-03 08:50:25 +0700

    update

---
+++ app/src/main/java/com/pencaksilat/app/pencaksilatapp/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.pencaksilat.app.pencaksilatapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        final int welcomeScreenDisplay = 4000; // 3000 = 3 detik
+        Thread welcomeThread = new Thread() {
+
+            int wait = 0;
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    while (wait < welcomeScreenDisplay) {
+                        sleep(100);
+                        wait += 100;
+                    }
+                } catch (Exception e) {
+                    System.out.println("EXc=" + e);
+
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        };
+
+        welcomeThread.start();
+    }
+}
