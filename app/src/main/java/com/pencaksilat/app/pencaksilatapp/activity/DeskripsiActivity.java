f5b4668b6b2d269e33628ca1156f369c9f501a01
Juris Vassa Ivandro <juris@unej.ac.id>
2018-11-06 06:48:13 +0700

    penambahan deskripsi activity

---
+++ app/src/main/java/com/pencaksilat/app/pencaksilatapp/activity/DeskripsiActivity.java
@@ -0,0 +1,13 @@
+package com.pencaksilat.app.pencaksilatapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeskripsiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deskripsi);
+    }
+}
